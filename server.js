2709de6ea3cf3ce3f3998b2e9034a12bb9580be2
Nitishshrestha1 <nitishshrestha159@gmail.com>
2025-12-07 13:17:40 -0500

    connected mongo db with the server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
 const filecontroll = require('./routes/filesRouter');
 const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 const app = express();
 
+// middleware to connect with mongodb
+connectDb();
+
+// required middleware
 app.use(express.json())
 app.use(express.static('public'));
 
-// user controll
+// user controll routers
 app.post('/api/register', (req,res) => {
     res.json({message: 'Register a new user'})
 })
@@ -15,9 +21,10 @@ app.post('/api/login', (req,res) => {
     res.json({message: 'Log in and receive a token'})
 })
 
-// file controll
+// file controll routers
 app.use('/api', filecontroll)
 
+// error handeling middleware
 app.use(errorHandler)
 
 app.listen(5000, () => {
